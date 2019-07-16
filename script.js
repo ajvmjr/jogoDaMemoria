cb3bfc2c6322fadf551464282a35d1afab8b366a
alex junior <alexjvm90@gmail.com>
2019-07-16 11:18:21 -0300

    adicionar imagens nas cartas

---
+++ script.js
@@ -0,0 +1,33 @@
+function embaralhar(lista){
+    let valorTemporario;
+    let indiceAleatorio;
+
+    for(let i = lista.length - 1; i !== 0; i--){
+        indiceAleatorio = Math.floor(Math.random() * i);
+
+        valorTemporario = lista[i];
+        lista[i] = lista[indiceAleatorio];
+        lista[indiceAleatorio] = valorTemporario;
+    }
+
+    return lista;
+}
+
+let cartas = document.querySelectorAll(".carta");
+
+let imagensSalvas = ["draven.png", "jhin.png", "kaisa.png", "lucian.png", "sivir.png", "tristana.png", "varus.png", "vayne.png"];
+
+let imagens = imagensSalvas.concat(imagensSalvas);
+
+for(let i = 0; i < cartas.length; i++){
+    cartas[i].style.backgroundImage = `url("images/${imagens[i]}")`;
+}
+
+setTimeout(function(){
+    for(let carta of cartas){
+        carta.style.backgroundImage = 'url("images/logo.png")';
+    }
+}, 3000);
+
+
+
