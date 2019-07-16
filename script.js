a57694adccfffbd730d1f6449127e365477c06f9
alex junior <alexjvm90@gmail.com>
2019-07-16 12:15:00 -0300

    começar lógica básica do jogo

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 function embaralhar(lista){
     let valorTemporario;
     let indiceAleatorio;
-
+    
     for(let i = lista.length - 1; i !== 0; i--){
         indiceAleatorio = Math.floor(Math.random() * i);
-
+        
         valorTemporario = lista[i];
         lista[i] = lista[indiceAleatorio];
         lista[indiceAleatorio] = valorTemporario;
     }
-
+    
     return lista;
 }
 
 let cartas = document.querySelectorAll(".carta");
+
+let cartaVirada = null;
 
 let imagensSalvas = ["draven.png", "jhin.png", "kaisa.png", "lucian.png", "sivir.png", "tristana.png", "varus.png", "vayne.png"];
 
@@ -28,9 +30,24 @@ for(let i = 0; i < cartas.length; i++){
 setTimeout(function(){
     for(let carta of cartas){
         carta.style.backgroundImage = 'url("images/logo.png")';
-        carta.onclick(){
-            carta.style.backgroundImage = `url("images/${imagens[Number(carta.id)]}")`;
-            carta.onclick = null;
+        carta.onclick = function(){
+            carta.style.backgroundImage = `url("images/${imagens[Number(carta.id)]})`;
+            if(cartaVirada && cartaVirada.id !== carta.id){
+                setTimeout(function(){
+                    if(cartaVirada.style.backgroundImage === carta.style.backgroundImage){
+                        cartaVirada.onclick = null;
+                        carta.onclick = null;
+                    }
+                    else{
+                        carta.style.backgroundImage = 'url("images/logo.png")';
+                        cartaVirada.style.backgroundImage = 'url("images/logo.png")';
+                    }
+                    cartaVirada = null;
+                }, 1500);
+            }
+            else{
+                cartaVirada = carta;
+            }
         }
     }
 }, 3000);
